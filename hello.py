905658f27af770669ade61b6fd54be8b572ba740
jomerp12 <jpaulino@terpmail.umd.edu>
2020-10-22 12:21:59 -0400

    resolved issue

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 def hello():
     print("Hello")
 
 def goodbye():
     print("Goodbye")
+=======
+def hello():
+    print("Hello")
+    
+def goodbye():
+    print("Good Bye")
+>>>>>>> parent of 9870c5c... hi
